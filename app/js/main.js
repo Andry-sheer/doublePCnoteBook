a40a1f5a4daac9d8ff94f55bc4c1db56cd319ee6
Andry <andry.sheremetew@gmail.com>
2024-04-07 05:06:59 +0300

    HW is completed

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,7 @@ const back = document.querySelector('.profile__buttonBack');
 const spContainer = document.querySelector('.spinnerContainer');
 const spin = document.querySelector('.spinner');
 const bodyContainer = document.querySelector('.bodyMain__container');
+const prContainer = document.querySelector('.profile__container');
 const email = document.querySelector('.bodyMain__email');
 const password = document.querySelector('.bodyMain__password');
 
@@ -11,18 +12,19 @@ function moveToProfile(){
   bodyContainer.style.display = 'none'
   spContainer.style.display = 'flex';
   spin.style.display = 'block';
-
   setTimeout(() => {
-    location.href = 'myProfile.html';
-  }, 4000);
+    location.href = 'myProfile.html'
+  }, 2000);
+  console.log(`e-mail = ${email.value} \npassword = ${password.value}`)
 };
 
 function moveToLogin(){
+  prContainer.style.display = 'none';
   spContainer.style.display = 'flex';
   spin.style.display = 'block';
-
   setTimeout(() => {
-    history.go(-1);
-  }, 4000);
-  
+    history.back()
+  }, 2000);
 };
+
+console.log(`ScreenUser: \nWidth: ${window.innerWidth}px \nHeight: ${window.innerHeight}px`);
