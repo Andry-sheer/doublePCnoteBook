6137529e338fccc5da87aa613923122b39955d9b
Andry <andry.sheremetew@gmail.com>
2024-04-10 02:46:27 +0300

    new branch forms

--- app/js/main.js
+++ app/js/main.js
@@ -2,34 +2,34 @@
 
 
 
-document.querySelector('#submit_person').addEventListener('submit', function(event){
-  event.preventDefault();
-  // console.log('submit')
-  const formData = new FormData(this);
-  const formObject = {};
+// document.querySelector('#submit_person').addEventListener('submit', function(event){
+//   event.preventDefault();
+//   // console.log('submit')
+//   const formData = new FormData(this);
+//   const formObject = {};
 
-  formData.forEach(function(value, key){
-    // console.log(value, key)
-    if (!formObject[key]) {
-      formObject[key] = value;
-    }
-    else {
-      formObject[key] = `${formObject[key]}, ${value}`
-    }
-})
+//   formData.forEach(function(value, key){
+//     // console.log(value, key)
+//     if (!formObject[key]) {
+//       formObject[key] = value;
+//     }
+//     else {
+//       formObject[key] = `${formObject[key]}, ${value}`
+//     }
+// })
     
-console.log(formObject)
+// console.log(formObject)
 
-  validate(formObject)
-})  
+//   validate(formObject)
+// })  
 
-  function validate(object){
+//   function validate(object){
     
-    if (object.password.length < 8){
-      document.querySelector('.passError').style.display = 'block';
-      document.querySelector('#submit_person').style.height = '400px';
-    }
-  };
+//     if (object.password.length < 8){
+//       document.querySelector('.passError').style.display = 'block';
+//       document.querySelector('#submit_person').style.height = '400px';
+//     }
+//   };
 
 
 
